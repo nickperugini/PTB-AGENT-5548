d420ea74fc83b5ef9a533e96d6fcaa457694f6e4
Nick Perugini <nickperugini10@gmail.com>
2025-05-30 13:55:05 -0400

    Create index.py

---
+++ lambda/lambda4/index.py
@@ -0,0 +1,12 @@
+import boto3, os
+
+def handler(event, context):
+    s3 = boto3.client("s3")
+    bucket = os.environ["BUCKET"]
+    # write a simple text file naming this Lambda
+    s3.put_object(
+        Bucket=bucket,
+        Key="lambda4.txt",       # change per function
+        Body="Hello from lambda1"  # change per function
+    )
+    return {"statusCode": 200, "body": "ok"}

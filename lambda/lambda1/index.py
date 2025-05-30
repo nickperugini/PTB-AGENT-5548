42fb6447b2802546d318867c4e704e576e8bde7f
Nick Perugini <nickperugini10@gmail.com>
2025-05-30 14:15:49 -0400

    Update index.py

--- lambda/lambda1/index.py
+++ lambda/lambda1/index.py
@@ -2,11 +2,24 @@ import boto3, os
 
 def handler(event, context):
     s3 = boto3.client("s3")
+    dynamo = boto3.resource("dynamodb")
     bucket = os.environ["BUCKET"]
-    # write a simple text file naming this Lambda
+    table_name = os.environ["DYNAMO_TABLE"]
+
+    # 1) write a simple text file to S3
     s3.put_object(
         Bucket=bucket,
-        Key="lambda1.txt",       # change per function
-        Body="Hello from lambda1"  # change per function
+        Key="lambda1.txt",
+        Body="Hello from lambda1"
     )
-    return {"statusCode": 200, "body": "ok"}
+
+    # 2) put an item into DynamoDB
+    table = dynamo.Table(table_name)
+    table.put_item(
+        Item={
+            "PK": "lambda1",
+            "timestamp": str(context.aws_request_id)
+        }
+    )
+
+    return {'statusCode': 200, 'body': 'ok'}
